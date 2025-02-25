bf5673335b0d756d65c67c6573b8751a0c2618e5
Anticubex <teofontana00@gmail.com>
2025-02-25 11:16:42 -0800

    Initial commit up to week 3 lab

---
+++ test.py
@@ -0,0 +1,13 @@
+class Pokemon:
+    def __init__(
+        self, max_hp, pokedex, name, pokemon_type, sp_atk, sp_def, atk, defense, speed
+    ):
+        self.max_hp = max_hp
+        self.pokedex = pokedex
+        self.name = name
+        self.pokemon_type = pokemon_type
+        self.sp_atk = sp_atk
+        self.sp_def = sp_def
+        self.atk = atk
+        self.defense = defense
+        self.speed = speed
