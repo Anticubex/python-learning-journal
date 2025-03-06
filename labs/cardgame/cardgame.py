c60dd932f06503c96546bbc55d518b978892efe9
Anticubex <teofontana00@gmail.com>
2025-03-05 19:24:03 -0800

    Day6 and a work-in progress cardgame

---
+++ labs/cardgame/cardgame.py
@@ -0,0 +1,30 @@
+"""
+A cardgame with 20 or so cards
+Inspired by a strange amalgamation of Factorio and Magic the Gathering
+"""
+
+from enum import Enum
+
+class Materials:
+    IRON_ORE = 0
+    COPPER_ORE = 1
+    IRON = 2
+    COPPER = 3
+    BASIC_CIRCUITS = 4
+    STEEL = 5
+    OIL = 6
+    PLASTIC = 7
+    ADVANCED_CIRCUITS = 8
+    SULFER = 9
+    ENGINE_UNIT = 10
+    LOW_DENSITY_STRUCTURE = 11
+
+
+
+# All cards are resources/materials, and some *can* be played--
+class MaterialCost:
+
+
+class Card:
+    
+
